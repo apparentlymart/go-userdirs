8e5094af75728ab6e783c20fc06698f2ac6de8ad
Martin Atkins <mart@degeneration.co.uk>
2019-05-11 12:34:51 -0700

    windowsbase: Fix typos in the implementation

--- windowsbase/impl.go
+++ windowsbase/impl.go
@@ -12,23 +12,23 @@ import (
 var (
 	shell32                  = windows.NewLazyDLL("Shell32.dll")
 	ole32                    = windows.NewLazyDLL("Ole32.dll")
-	procSHGetKnownFolderPath = modShell32.NewProc("SHGetKnownFolderPath")
-	procCoTaskMemFree        = modOle32.NewProc("CoTaskMemFree")
+	procSHGetKnownFolderPath = shell32.NewProc("SHGetKnownFolderPath")
+	procCoTaskMemFree        = ole32.NewProc("CoTaskMemFree")
 )
 
 func knownFolderDir(fid *FolderID) (string, error) {
 	var path uintptr
-	err := SHGetKnownFolderPath(fid, 0, 0, &path)
+	err := shGetKnownFolderPath(fid, 0, 0, &path)
 	if err != nil {
 		return "", err
 	}
-	defer CoTaskMemFree(path)
+	defer coTaskMemFree(path)
 	dir := syscall.UTF16ToString((*[1 << 16]uint16)(unsafe.Pointer(path))[:])
 	return dir, nil
 }
 
 func shGetKnownFolderPath(fid *FolderID, dwFlags uint32, hToken syscall.Handle, pszPath *uintptr) (retval error) {
-	r0, _, _ := procSHGetKnownFolderPath.Call(uintptr(unsafe.Pointer(rfid)), uintptr(dwFlags), uintptr(hToken), uintptr(unsafe.Pointer(pszPath)), 0, 0)
+	r0, _, _ := procSHGetKnownFolderPath.Call(uintptr(unsafe.Pointer(fid)), uintptr(dwFlags), uintptr(hToken), uintptr(unsafe.Pointer(pszPath)), 0, 0)
 	if r0 != 0 {
 		return syscall.Errno(r0)
 	}
