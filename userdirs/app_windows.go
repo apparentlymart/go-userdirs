c7962ef2d020bcf9734afe80a7dccaf3bf6140f1
Martin Atkins <mart@degeneration.co.uk>
2019-05-11 11:33:03 -0700

    userdirs: Implementations for Unix, Windows and Mac OS

---
+++ userdirs/app_windows.go
@@ -0,0 +1,23 @@
+// +build windows
+
+import (
+	"path/filepath"
+
+	"github.com/apparentlymart/go-userdirs/windowsbase"
+)
+
+func supportedOS() bool {
+	return true
+}
+
+func forApp(name string, vendor string, bundleID string) Dirs {
+	subDir := filepath.Join(vendor, name)
+	roamingDir := filepath.Join(windowsbase.RoamingAppDataDir(), subDir)
+	localDir := filepath.Join(windowsbase.LocalAppDataDir(), subDir)
+
+	return Dirs{
+		ConfigDirs: []string{roamingDir},
+		DataDirs:   []string{roamingDir},
+		CacheDir:   localDir,
+	}
+}
---
+++ userdirs/dirs.go
@@ -0,0 +1,47 @@
+package userdirs
+
+// Dirs represents a set of directory paths with different purposes.
+type Dirs struct {
+	// ConfigDirs is a list, in preference order, of directory paths to search
+	// for configuration files.
+	//
+	// The list must always contain at least one element, and its first element
+	// is the directory where any new configuration files should be written.
+	//
+	// On some systems, ConfigDirs and DataDirs may overlap, so applications
+	// which scan the contents of the configuration directories should impose
+	// some additional filtering to distinguish configuration files from data
+	// files.
+	ConfigDirs []string
+
+	// DataDirs is a list, in preference order, of directory paths to search for
+	// data files.
+	//
+	// The list must always contain at least one element, and its first element
+	// is the directory where any new data files should be written.
+	//
+	// On some systems, ConfigDirs and DataDirs may overlap, so applications
+	// which scan the contents of the data directories should impose some
+	// additional filtering to distinguish data files from configuration files.
+	DataDirs []string
+
+	// CacheDir is the path of a single directory that can be used for temporary
+	// cache data.
+	//
+	// The cache is suitable only for data that the calling application could
+	// recreate if lost. Any file or directory under this prefix may be deleted
+	// at any time by other software.
+	CacheDir string
+}
+
+// ConfigHome returns the path for the directory where any new configuration
+// files should be written.
+func (d Dirs) ConfigHome() string {
+	return d.ConfigDirs[0]
+}
+
+// DataHome returns the path for the directory where any new configuration
+// files should be written.
+func (d Dirs) DataHome() string {
+	return d.DataDirs[0]
+}
